989d3d90025d41eebb2feafd585077888d042568
Arseny Gusev <arseny.gusev06@net-ustu.ru>
2020-05-28 18:05:12 +0500

    Formatted Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,69 +1,67 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
 import {MatPaginatorModule} from '@angular/material/paginator';
-import {DataTableComponent } from './pages/shows/data-table/data-table.component';
+import {DataTableComponent} from './pages/shows/data-table/data-table.component';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatSortModule} from '@angular/material/sort';
 import {FormsModule} from '@angular/forms';
-import { PaginatorComponent } from './pages/shows/paginator/paginator.component';
-import { PaginationPipe } from './shared/pipes/pagination.pipe';
-import { FilterComponent } from './pages/shows/filter/filter.component';
+import {PaginatorComponent} from './pages/shows/paginator/paginator.component';
+import {PaginationPipe} from './shared/pipes/pagination.pipe';
+import {FilterComponent} from './pages/shows/filter/filter.component';
 import {StoreModule} from '@ngrx/store';
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {EffectsModule} from "@ngrx/effects";
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {EffectsModule} from '@ngrx/effects';
 import {environment} from '../environments/environment';
-import {showsReducer} from './store/shows/reducer';
 import {ShowsEffects} from './store/shows/effects';
 import {reducers} from './store/root.reducers';
 import {AppContainer} from './app.container';
 import {FilterContainer} from './pages/shows/filter/filter.container';
-import {DataTableContainer} from './pages/shows/data-table/data-table.container';
+import {DataTableContainerComponent} from './pages/shows/data-table/data-table.container';
 import {PaginatorContainer} from './pages/shows/paginator/paginator.container';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    AppContainer,
-    DataTableComponent,
-    DataTableContainer,
-    PaginatorComponent,
-    PaginatorContainer,
-    PaginationPipe,
-    FilterComponent,
-    FilterContainer
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatTableModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatSelectModule,
-    MatPaginatorModule,
-    MatChipsModule,
-    MatSortModule,
-    FormsModule,
-    StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([ShowsEffects]),
-    StoreDevtoolsModule.instrument({
-      name: 'TV App DevTools',
-      maxAge: 25,
-      logOnly: environment.production
-    })
-  ],
-  providers: [],
-  bootstrap: [ AppContainer ]
+	declarations: [
+		AppComponent,
+		AppContainer,
+		DataTableComponent,
+		DataTableContainerComponent,
+		PaginatorComponent,
+		PaginatorContainer,
+		PaginationPipe,
+		FilterComponent,
+		FilterContainer
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		MatTableModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatSelectModule,
+		MatPaginatorModule,
+		MatChipsModule,
+		MatSortModule,
+		FormsModule,
+		StoreModule.forRoot(reducers),
+		EffectsModule.forRoot([ShowsEffects]),
+		StoreDevtoolsModule.instrument({
+			name: 'TV App DevTools',
+			maxAge: 25,
+			logOnly: environment.production
+		})
+	],
+	providers: [],
+	bootstrap: [AppContainer]
 })
 export class AppModule {}
--- src/app/pages/shows/data-table/data-table.container.ts
+++ src/app/pages/shows/data-table/data-table.container.ts
@@ -2,29 +2,27 @@ import {Component, Input} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../../store/root.reducers';
 import {Observable} from 'rxjs';
-import {ShowInterface} from '../../../shared/interfaces/show.interface'
-import {getCurrentPageShows, getSelectedGenre, getShows} from '../../../store/shows/selectors';
+import {ShowInterface} from '../../../shared/interfaces/show.interface';
+import {getCurrentPageShows} from '../../../store/shows/selectors';
 import {ChangeSorting, SelectGenre} from '../../../store/shows/actions';
 
 @Component({
-  selector: 'app-data-table-container',
-  template: '<app-data-table  [shows]="shows$ | async" (genreSelected)="onChangeSelectedGenre($event)" (sortChange)="onSortChange($event)"></app-data-table>'
+	selector: 'app-data-table-container',
+	template:
+		'<app-data-table  [shows]="shows$ | async" (genreSelected)="onChangeSelectedGenre($event)" (sortChange)="onSortChange($event)"></app-data-table>'
 })
+export class DataTableContainerComponent {
+	shows$: Observable<ShowInterface[]>;
 
-export class DataTableContainer {
+	constructor(private store: Store<AppState>) {
+		this.shows$ = this.store.select(getCurrentPageShows);
+	}
 
+	onChangeSelectedGenre(genre) {
+		this.store.dispatch(new SelectGenre(genre));
+	}
 
-  shows$: Observable<ShowInterface[]>
-
-  constructor(private store: Store<AppState>) {
-    this.shows$ = this.store.select(getCurrentPageShows);
-  }
-
-  onChangeSelectedGenre(genre) {
-    this.store.dispatch(new SelectGenre(genre))
-  }
-
-  onSortChange(sorting) {
-    this.store.dispatch(new ChangeSorting(sorting))
-  }
+	onSortChange(sorting) {
+		this.store.dispatch(new ChangeSorting(sorting));
+	}
 }
